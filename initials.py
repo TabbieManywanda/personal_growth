e11324927a3ff53e132d7e1f0159b8059806e457
TabbieManywanda <achiengtabitha@gmail.com>
2023-01-23 12:17:24 +0300

    ASCII art of my name initials

---
+++ initials.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+#My name: Tabitha Achieng Manywanda
+#ASCII art of my name initials
+
+print('T T T T T    A      M          M')
+print('    T     A     A   M  M    M  M')
+print('    T    A       A  M   M  M   M')
+print('    T    A A A A A  M    M     M')
+print('    T    A       A  M          M')
+print('    T    A       A  M          M')
+print('    T    A       A  M          M')
